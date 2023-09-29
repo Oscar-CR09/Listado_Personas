d57a91d015c4481488379bb5cc6d7594d19d4309
Oscar <oscarcr189@outlook.com>
2023-09-28 23:03:39 -0600

    Listado de Personas App2

--- src/Persona.java
+++ src/Persona.java
@@ -19,4 +19,51 @@ public class Persona {
         this.email = email;
 
     }
+
+    public int getId(){
+        return id;
+    }
+    public void setId(){
+        this.id=id;
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", tel='" + tel + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Persona persona1 = new Persona("Juan Perez", "55669823","juan@mail.com");
+        System.out.println(persona1.toString());
+    }
 }
